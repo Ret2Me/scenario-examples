63c4f983d3f6a90f59dd1b65e0ba7803b4f96c53
Filip "Ret2Me" Poplewski <37419029+Ret2Me@users.noreply.github.com>
2025-07-20 21:09:46 +0200

    Update v.sh

--- CKA/pod-filter/step1/v.sh
+++ CKA/pod-filter/step1/v.sh
@@ -4,7 +4,7 @@
 script_path="pod-filter.sh"
 
 # Define the commands to search for
-commands=("kubectl get pod nginx-pod -o=jsonpath='{.metadata.labels.application}'" "kubectl get pods nginx-pod -o=jsonpath='{.metadata.labels.application}'")
+commands=("kubectl get pod nginx-pod -o=jsonpath='{.metadata.labels.application}'" "kubectl get pods nginx-pod -o=jsonpath='{.metadata.labels.application}'" 'kubectl get pod nginx-pod -o=jsonpath="{.metadata.labels.application}"' 'kubectl get pods nginx-pod -o=jsonpath="{.metadata.labels.application}"')
 
 # Flag to indicate if at least one command is found
 found=false
